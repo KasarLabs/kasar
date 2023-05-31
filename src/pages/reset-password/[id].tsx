743cc191227087570208929dacf5f855414916c4
jb1011 <demenet.jb@hotmail.fr>
2023-05-31 12:50:12 +0200

    new page added to recover password

---
+++ src/pages/reset-password/[id].tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import styled from 'styled-components';
+import { TextBold } from '@/components/s-components/Titles';
+import { Input } from '@/components/s-components/Input';
+import { ButtonPrimary } from '@/components/s-components/Button';
+
+const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Flex = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+function Index() {
+  const router = useRouter();
+  const token = router.query.id;
+  const [newPassword, setNewPassword] = useState('');
+  const [sent, setSent] = useState(false);
+
+  const sendData = async () => {
+    if (token) {
+      try {
+        console.log('clicked')
+        const { data } = await axios.post(`http://localhost:8000/api/reset-password`,
+          {
+            resetPasswordLink: token,
+            newPassword: newPassword,
+          });
+        setSent(true)
+      } catch (err) {
+        console.log('error data', err)
+      }
+    }
+  }
+  return (
+    <Main>
+      {sent ?
+        <>
+          <TextBold>
+            Password is not reset. Go back to the app to login with your new password.
+          </TextBold>
+        </>
+        :
+        <>
+          <TextBold>
+            Reset your password
+          </TextBold>
+          <Flex>
+            <Input type='password' value={newPassword} onChange={e => setNewPassword(e.target.value)} placeholder='Enter your new password' />
+            <ButtonPrimary onClick={sendData}>Reset</ButtonPrimary>
+          </Flex>
+        </>
+      }
+    </Main>
+
+  )
+}
+
+export default Index
